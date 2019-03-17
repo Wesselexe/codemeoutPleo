594a61c8303c7c068b33bd27e7e68cc6583a297e
wesselexe <Ledigmand1>
2019-03-17 16:26:00 +0100

    Once more

--- src/socket.js
+++ src/socket.js
@@ -7,6 +7,6 @@ export const socket = () => {
 };
 
 export const initSocket = callback => {
-  _socket = io("https://codemeout.netlify.com/");
+  _socket = io("https://codemeout.netlify.com");
   callback();
 };
